b8f5582871483e015af8a2d5a84b2133f46e546a
lupfeliz <lupfeliz@gmail.com>
2024-11-08 09:45:08 +0900

    httputil 을 ntiple-utils 에 있는기능으로 대체

--- src/test/java/com/ntiple/SimpleTest.java
+++ src/test/java/com/ntiple/SimpleTest.java
@@ -35,15 +35,17 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.SimpleScriptContext;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.DERNull;
 import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import com.ntiple.TestUtil.TestLevel;
+import com.ntiple.commons.HttpUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -68,82 +70,17 @@ public class SimpleTest {
     }
     {
       Pattern PTN_NL = Pattern.compile("[\\r\\n][\\\\]", Pattern.MULTILINE);
-      // Pattern PTN_NL = Pattern.compile("\\\\", Pattern.MULTILINE);
       String str = "function $component_input(app){const CInput={template:`<input\r" +
         "\\ class=\"form-control\"\r" +
         "\\:vrules=\"\r" +
         "\\ data-test=\"OK\"\r" +
         "\\/>`,name:\"c-input\",props:{},data:function(){return{};},mounted:async function(){}};app.component(\"c-input\",CInput);};\r";
-      // String str = readAsString(file("/home/coder/documents/tmp/c-input.js"));
       log.debug("STR:{}", str);
       log.debug("--------------------------------------------------------------------------------");
       log.debug("STR:{}", PTN_NL.matcher(str).replaceAll(""));
     }
     assertTrue(true);
   }
-
-  @Test public void testByNashornScript() throws Exception {
-    if (!TestUtil.isEnabled("testByNashornScript", TestLevel.MANUAL)) { return; }
-    // final String LANG_VER = "TypeScript.LanguageVersion.EcmaScript5";
-    // ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-    // engine.eval("print('OK');");
-    // engine.eval("var Sass = require('./sass.sync-0.11.1.min.js');");
-    // engine.eval(
-    //   "const Sass = require('./sass.sync-0.11.1.min.js');\r" +
-    //   "Sass.compile(`div { border: 1px solid #f00; > div { background: #f00; } }`, function(r) { console.log(r.text); });"
-    // );
-    // if (launchForBuild) { return; }
-    // ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
-    // ScriptContext context = new SimpleScriptContext();
-    // Bindings binding = engine.createBindings();
-    // engine.setContext(context);
-    // List<Integer> scopes = context.getScopes();
-    // context.setBindings(binding, scopes.get(0));
-    // Reader reader = null;
-    // //ScriptContext context = null;
-    // try {
-    //   reader = new InputStreamReader(Application.class.getClassLoader().getResource("META-INF/" + TYPESCRIPT_COMPILER).openConnection().getInputStream());
-    //   engine.eval(reader, context);
-    //   reader.close();
-    //   reader = new InputStreamReader(Application.class.getClassLoader().getResource("META-INF/" + COMPILER_WRAPPER).openConnection().getInputStream());
-    //   engine.eval(reader, context);
-    //   File targetScript = new File("src/test/resources/test.ts");
-    //   binding.put("input", targetScript.getAbsolutePath());
-    //   binding.put("contextName", "");
-    //   binding.put("ver", LANG_VER);
-    //   String script = "var result;"+
-    //   "result = compilerWrapper.compile(input, ver, contextName);"+
-    //   "";
-    //   engine.eval(script, context);
-    //   assertTrue(true);
-    // } finally {
-    //   if (reader != null) try { reader.close(); } catch (Exception ignore) { }
-    // }
-  }
-
-  // @Test
-  // public void testSassCompile() throws Exception {
-  //   if (!TestUtil.isEnabled("testSassCompile", TestLevel.MANUAL)) { return; }
-  //   try (SassCompiler sassCompiler = SassCompilerFactory.bundled()) {
-  //     String content = "div { border: 1px solid #ccc; > div { background: #f00; } }";
-  //     // CompileSuccess res = sassCompiler.compileCssString(content);
-  //     // CompileSuccess res = sassCompiler.compileSassString(content);
-  //     CompileSuccess res = sassCompiler.compileScssString(content);
-  //     // log.debug("RESULT:\n{}", res.getCompileResponse());
-  //     log.debug("RESULT:\n{}", res.getCss());
-  //   } catch (Exception e) {
-  //     log.debug("E:", e);
-  //   }
-  //   // try (SassCompiler sassCompiler = SassCompilerFactory.bundled()) {
-  //   //   sassCompiler.registerImporter(new WebjarsImporter().autoCanonicalize());
-  //   //   URL resource = getClass().getResource("/custom-bootstrap.scss");
-  //   //   CompileSuccess compileSuccess = sassCompiler.compile(resource);
-  //   //   // custom Bootstrap css
-  //   //   String css = compileSuccess.getCss();
-  //   // } catch (Exception e) {
-  //   //   log.debug("E:", e);
-  //   // }
-  // }
 
   @Test
   public void testMinifyJS() throws Exception {
@@ -153,7 +90,6 @@ public class SimpleTest {
     context.getBindings(ScriptContext.GLOBAL_SCOPE);
     context.setBindings(engine.createBindings(), ScriptContext.GLOBAL_SCOPE);
     Bindings bindings = context.getBindings(ScriptContext.GLOBAL_SCOPE); 
-    // Bindings bindings = new SimpleBindings();
     bindings.put("console", System.console());
     engine.setContext(context);
     BufferedReader reader = null;
@@ -182,122 +118,6 @@ public class SimpleTest {
       safeclose(reader);
       safeclose(writer);
     }
-  }
-
-  // @Test
-  // public void testYUICompressor() throws Exception {
-  //   if (!TestUtil.isEnabled("testYUICompressor", TestLevel.MANUAL)) { return; }
-  //   BufferedReader reader = null;
-  //   StringWriter out = new StringWriter();
-  //   try {
-  //     reader = reader(file("/home/coder/documents/tmp/minify-7196260321170045552.script"), UTF8);
-  //     JavaScriptCompressor compressor = new JavaScriptCompressor(reader, new ErrorReporter() {
-  //       @Override public void error(String arg0, String arg1, int arg2, String arg3, int arg4) {
-  //         log.debug("ERROR:");
-  //       }
-  //       @Override public EvaluatorException runtimeError(String arg0, String arg1, int arg2, String arg3, int arg4) {
-  //         log.debug("ERROR:");
-  //         return null;
-  //       }
-  //       @Override public void warning(String arg0, String arg1, int arg2, String arg3, int arg4) { }
-  //     });
-  //     int linebreakpos = -1;
-  //     boolean munge = false;
-  //     boolean preserveAllSemiColons = false;
-  //     boolean disableOptimizations = false;
-  //     boolean verbose = true;
-  //     compressor.compress(out, linebreakpos, munge, verbose,
-  //       preserveAllSemiColons, disableOptimizations);
-  //     log.debug("OUT:{}", out.toString());
-  //   } catch (Exception e) {
-  //     log.debug("E:", e);
-  //   } finally {
-  //   }
-  // }
-
-  // @Test
-  // public void testClosureCompiler() throws Exception {
-  //   com.google.javascript.jscomp.Compiler compiler = new com.google.javascript.jscomp.Compiler();
-  //   CompilerOptions options = new CompilerOptions();
-  //   WarningLevel warningLevel = WarningLevel.QUIET;
-  //   options.setCodingConvention(CodingConventions.getDefault());
-  //   options.setOutputCharset(UTF8);
-  //   // options.setOutputCharset(Charset.forName(UTF8));
-  //   warningLevel.setOptionsForWarningLevel(options);
-  //   // DEFAULT_COMPILATION_LEVEL.setOptionsForCompilationLevel(options);
-  //   File file = file("/home/coder/documents/tmp/minify-7196260321170045552.script");
-  //   compiler.disableThreads();
-  //   final List<SourceFile> defaultExterns = CommandLineRunner.getDefaultExterns();
-  //   // List<SourceFile> defaultExterns = CommandLineRunner.getBuiltinExterns(null);
-  //   List<SourceFile> jsSourceFiles = new ArrayList<>();
-  //   jsSourceFiles.add(SourceFile.fromFile(file.getAbsolutePath(), Charset.forName("UTF-8")));
-  //   Result result = compiler.compile(defaultExterns, jsSourceFiles, options);
-  //   log.debug("RESULT:{}", result);
-  // }
-
-  // @Test
-  // public void testBabel() throws Exception {
-  //   if (!TestUtil.isEnabled("testBabel", TestLevel.MANUAL)) { return; }
-  //   ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-  //   ScriptContext context = new SimpleScriptContext();
-  //   context.getBindings(ScriptContext.GLOBAL_SCOPE);
-  //   context.setBindings(engine.createBindings(), ScriptContext.GLOBAL_SCOPE);
-  //   Bindings bindings = context.getBindings(ScriptContext.GLOBAL_SCOPE); 
-  //   // Bindings bindings = new SimpleBindings();
-  //   bindings.put("console", System.console());
-  //   engine.setContext(context);
-  //   BufferedReader reader = null;
-  //   Writer writer = null;
-  //   File scrFile = null;
-  //   File srcFile = null;
-  //   try {
-  //     Invocable invocable = (Invocable) engine;
-  //     scrFile = TestUtil.getResource(Application.class, "/scripts/babel-core-6.1.19.min.js");
-  //     reader = reader(istream(scrFile), UTF8);
-  //     engine.eval(reader);
-  //     safeclose(reader);
-  //     // scrFile = TestUtil.getResource(Application.class, "/scripts/do-minify.js");
-  //     // reader = reader(istream(scrFile), UTF8);
-  //     // engine.eval(reader);
-  //     // safeclose(reader);
-  //     // srcFile = file("/home/coder/documents/tmp/test.js");
-  //     // String content = readAsString(srcFile);
-  //     // content = content.replaceAll("`", "｀");
-  //     // Object obj = invocable.invokeFunction("minifyCode", content);
-  //     // if (obj != null) {
-  //     //   content = String.valueOf(obj).replaceAll("｀", "`");
-  //     // }
-  //     // log.debug("RESULT:{}", content);
-  //   } finally {
-  //     safeclose(reader);
-  //     safeclose(writer);
-  //   }
-  // }
-
-  // @Test
-  // public void testPhantomJS() throws Exception {
-  //   if (!TestUtil.isEnabled("testPhantomJS", TestLevel.MANUAL)) { return; }
-  //   try {
-  //     String str = "console.log('OK');";
-  //     InputStream script = new ByteArrayInputStream(str.getBytes());
-  //     PhantomJSOptions option = PhantomJSOptions.DEFAULT.withHelp(true);
-  //     PhantomJS.exec(script, option);
-  //   } catch (Exception e) {
-  //     log.debug("E:", e);
-  //   }
-  // }
-
-  @Test
-  public void testCast() throws Exception {
-    if (!TestUtil.isEnabled("testCast", TestLevel.MANUAL)) { return; }
-    JSONObject prm = null;
-    prm = testCastImpl(prm);
-  }
-
-  public static <T> T testCastImpl(T t) throws Exception {
-    T ret = t;
-    log.debug("CHECK:{} / {}", t, t instanceof JSONObject);
-    return ret;
   }
 
   @Test
@@ -369,23 +189,17 @@ public class SimpleTest {
       dec = decrypt(key, enc);
       log.debug("DECRYPT-PUB:{} -> {}", enc, dec);
     }
-    // KeyFactory keyFactory2 = KeyFactory.getInstance("RSA");
-    // byte[] bPublicKey2 = Base64.getDecoder().decode(pubk.getBytes());
-    // PublicKey publicKey2 = null;
-    // byte[] bPrivateKey2 = Base64.getDecoder().decode(prvk.getBytes());
-    // PrivateKey privateKey2 = null;
-    // X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(bPublicKey2);
-    // publicKey2 = keyFactory2.generatePublic(publicKeySpec);
-    // DerInputStream derReader = new DerInputStream(Base64.getDecoder().decode(prvks));
-    // DerValue[] seq = derReader.getSequence(0);
-    // skip version seq[0];
-    // BigInteger modulus = seq[1].getBigInteger();
-    // BigInteger publicExp = seq[2].getBigInteger();
-    // BigInteger privateExp = seq[3].getBigInteger();
-    // BigInteger prime1 = seq[4].getBigInteger();
-    // BigInteger prime2 = seq[5].getBigInteger();
-    // BigInteger exp1 = seq[6].getBigInteger();
-    // BigInteger exp2 = seq[7].getBigInteger();
-    // BigInteger crtCoef = seq[8].getBigInteger();
+  }
+
+  @Test
+  public void testHttpClient() throws Exception {
+    if (!TestUtil.isEnabled("testHttpClient", TestLevel.MANUAL)) { return; }
+    HttpResponse resp = HttpUtil.execute(
+      HttpUtil.httpClient(), null,
+      new HttpGet("https://gitlab.ntiple.com"),
+      null,
+      HttpResponse.class);
+    String html = HttpUtil.respContentStr(resp);
+    log.debug("CHECK:{}", html);
   }
 }
